4e9d69ab2be458c898f97f507107d99542e6b6cd
David Benoit <d.benoit@hull.ac.uk>
2023-03-01 23:29:17 +0000

    Update transforms.py

--- steps/transforms.py
+++ steps/transforms.py
@@ -82,9 +82,8 @@ def transform_interaction_operator(
         #extract H1 and H2 from PH hamiltonian
         (phh1,phh2)=rearrange_both(newferOp.h1,newferOp.h2,active_fermions)
         #Flip sign for H2
-        phh2*=-1
-       # input_operator = InteractionOperator(energy_shift, phh1, phh2)
-        input_operator = InteractionOperator(-energy_shift, phh1, phh2)
+        #phh2*=-1
+        input_operator = InteractionOperator(energy_shift, phh1, phh2)
         #input_operator = InteractionOperator(0.0, phh1, phh2)
         print(input_operator)
         input_operator = get_fermion_operator(input_operator)
