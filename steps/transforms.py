089254577e592401f680a47f9e5375a374dc5ed0
David Benoit <d.benoit@hull.ac.uk>
2023-03-15 13:44:53 +0000

    Update transforms.py

--- steps/transforms.py
+++ steps/transforms.py
@@ -27,12 +27,12 @@ def rearrange_both(array,array2,occ):
     # NOW: Up then down reordering, given the operator has the default even-odd ordering. 
     # Otherwise this function will reorder indices where all even
     # indices now come before odd indices.
-    # Example: 0,1,2,3,4,5 -> 0,2,4,1,3,5
+    # Example: 0,1,2,3,4,5 -> 0,2,4,1,3,5 (function logic taken from openfermion utils/indexing.py 
     #Performs both one-body and two-body intergral matrices rearrengments - checked on H2/STO-3G 
     #with original qiskit code
     idx=[]
     num_qubits=len(array)  
-    halfway = int(numpy.ceil(num_qubits / 2.))
+    halfway = int(np.ceil(num_qubits / 2.))
 
     for i in range(num_qubits):
         if i % 2 == 0:
